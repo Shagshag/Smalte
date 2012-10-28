a73aa310057c6fe4779434de4bc5494c79108516
Julien Breux <julien.breux@gmail.com>
2012-10-28 11:30:28 +0100

    Add application bootstrap

---
+++ bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ *	(c) Smalte - 2012 ~ Until the end of the world...
+ *
+ *	Julien Breux <julien@smalte.org>
+ *	Fabien Serny <fabien@smalte.org>
+ *	Grégoire Poulain <gregoire@smalte.org>
+ *	Alain Folletete <alain@smalte.org>
+ *	Raphaël Malié <raphael@smalte.org>
+ *
+ *	Thanks a lot to our community!
+ *
+ *	Read LICENCE file for more information.
+ */
+
+// Use composer bootstrap (PSR-0 compliant)
+$loader = require_once dirname(__FILE__).'/libraries/autoload.php';
