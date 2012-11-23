777d5b7c4389c89f32869f7e8c37c866dbd2300f
Fabien Serny <fabien@23prod.fr>
2012-11-23 17:27:16 +0100

    Add license

--- entities/repositories/User.php
+++ entities/repositories/User.php
@@ -1,4 +1,19 @@
 <?php
+
+/*
+ * (c) Smalte - 2012 ~ Until the end of the world...
+ *
+ * Julien Breux <julien@smalte.org>
+ * Fabien Serny <fabien@smalte.org>
+ * Grégoire Poulain <gregoire@smalte.org>
+ * Alain Folletete <alain@smalte.org>
+ * Raphaël Malié <raphael@smalte.org>
+ *
+ * Thanks a lot to our community!
+ *
+ * Read LICENSE.md file for more information.
+ */
+
 namespace Entities\Repositories;
 
 use Smalte\ORM\Repository;
