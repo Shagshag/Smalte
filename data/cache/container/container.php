c9c5a0a7d89841687cdfb4fdeac17b08abe84d9d
Alain Folletete <folletete.alain@gmail.com>
2012-11-29 01:16:55 +0100

    Fix container cache

--- data/cache/container/container.php
+++ data/cache/container/container.php
@@ -11,9 +11,7 @@ $container['mailing'] = function ($c) {
 						return $object;
 					};
 $container['templating'] = function ($c) {
-						$object = new Smalte\Template\Template(array (
-  0 => new $c['templating.adapater'](),
-));
+						$object = new Smalte\Template\Template(new $c['templating.adapater']());
 						call_user_func_array(array($object, 'setTemplateDirectory'), array (
   0 => $c['templating.directory'],
 ));
