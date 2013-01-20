5e81aba201aa925a3572595a6f797a0e50cf0e24
Alain Folletete <folletete.alain@gmail.com>
2013-01-20 23:59:37 +0100

    Create new error controller

---
+++ controllers/frontoffice/Error.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * (c) Smalte - 2012 ~ Until the end of the world...
+ *
+ * Julien Breux <julien@smalte.org>
+ * Fabien Serny <fabien@smalte.org>
+ * Grégoire Poulain <gregoire@smalte.org>
+ * Alain Folletete <alain@smalte.org>
+ * Raphaël Malié <raphael@smalte.org>
+ *
+ * Thanks a lot to our community!
+ *
+ * Read LICENCE file for more information.
+ */
+
+namespace Controllers\FrontOffice;
+
+use \Smalte\Controller\Controller;
+
+class Error extends Controller
+{
+	public function display404Action()
+	{
+		// Nothing !
+	}
+}
+
