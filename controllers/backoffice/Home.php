420378bc32483082b001c58d4e7bb3db8c3bca18
Alain Folletete <folletete.alain@gmail.com>
2013-01-21 00:17:25 +0100

    Add first back office controller

---
+++ controllers/backoffice/Home.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * (c) Smalte - 2012 ~ Until the end of the world...
+ *
+ * Julien Breux <julien@smalte.org>
+ * Fabien Serny <fabien@smalte.org>
+ * Grégoire Poulain <gregoire@smalte.org>
+ * Alain Folletete <alain@smalte.org>
+ * Raphaël Malié <raphael@smalte.org>
+ *
+ * Thanks a lot to our community!
+ *
+ * Read LICENCE file for more information.
+ */
+
+namespace Controllers\BackOffice;
+
+use \Smalte\Controller\Controller;
+
+class Home extends Controller
+{
+	public function indexAction()
+	{
+		// Set template vars
+		$templateVars = array(
+			'name' => 'John Doe',
+		);
+
+		$this->setTemplateVars($templateVars);
+	}
+}
+
