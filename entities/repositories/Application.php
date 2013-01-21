36716a9bb832b3a814e1fd2a748a1a44e164d18f
Julien Breux <julien.breux@gmail.com>
2013-01-21 11:32:46 +0100

    Add getDefault and getByPrefix methods

--- entities/repositories/Application.php
+++ entities/repositories/Application.php
@@ -20,4 +20,34 @@ use Smalte\ORM\Work\Repository;
 
 class Application extends Repository
 {
+	/**
+	 * Get default application
+	 *
+	 * @param bool $enabled Applications enabled
+	 *
+	 * @return Application
+	 */
+	public function getDefault($enabled = true)
+	{
+		return $this->findOneBy(array(
+			'isDefault'	=> true,
+			'isEnabled'	=> $enabled,
+		));
+	}
+
+	/**
+	 * Get application by prefix
+	 *
+	 * @param string $prefix Prefix
+	 * @param bool $enabled Applications enabled
+	 *
+	 * @return Application
+	 */
+	public function getByPrefix($prefix, $enabled = true)
+	{
+		return $this->findOneBy(array(
+			'prefix'	=> $prefix,
+			'isEnabled'	=> $enabled,
+		));
+	}
 }
