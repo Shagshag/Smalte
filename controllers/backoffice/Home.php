55af1369e29d38a5bef9055707369168f1cd2ba6
Alain Folletete <Alain@Mac-mini-de-Locadmin.local>
2013-02-07 10:38:28 +0100

    Update comments on header of files

--- controllers/backoffice/Home.php
+++ controllers/backoffice/Home.php
@@ -3,15 +3,11 @@
 /*
  * (c) Smalte - 2013 ~ Until the end of the world...
  *
- * Julien Breux <julien@smalte.org>
- * Fabien Serny <fabien@smalte.org>
- * Grégoire Poulain <gregoire@smalte.org>
- * Alain Folletete <alain@smalte.org>
- * Raphaël Malié <raphael@smalte.org>
- *
  * Thanks a lot to our community!
  *
- * Read LICENCE file for more information.
+ * @link http://smalte.org Official website
+ * @link http://github.com/Smalte/Smalte For the source repository
+ * @license Read LICENSE.md file for more information.
  */
 
 namespace Controllers\BackOffice;
